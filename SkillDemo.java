26c1ad9f714ba311a099a483574e04b54ce1f030
HaohanZou <pista5534@gmail.com>
2022-04-30 10:28:15 -0700

    Add a new method and a new test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,5 +1,8 @@
 public class SkillDemo{
 
-
+    public static int multiply(int x, int y){
+        int product = x*y;
+        return product;
+    }
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest{
+    // This file is used to test method multiply
+
+    @Test
+    public void test1(){
+        assertEquals(3, SkillDemo.multiply(1,2)); // This test should fail!
+    }
+
+
+}
